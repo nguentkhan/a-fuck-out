8b9a799d150e5fb5531c8fc148f6e632e7dff2c2
nguentkhan <89307836+nguentkhan@users.noreply.github.com>
2021-10-17 20:28:36 +0300

    Update So-The.php

--- So-The.php
+++ So-The.php
@@ -1,1 +1,22 @@
-
+<!doctype html>
+ <html>
+<head>
+  <title>Dữ Liệu Bạn Nhập</title>
+</head>
+<body>
+<h1>Dữ Liệu Bạn Nhập Đã Bị Chúng Tôi Đánh Cắp Vui lòng báo công an=))</h1>
+   <?php
+ $1 = $_GET['1']
+ $2 = $_GET['2']
+ $3 = $_GET['3']
+ $4 = $_GET['4']
+ $5 = $_GET['5']
+?>
+ echo 'Số Thẻ: ' .1;
+ echo 'Tên Người Dùng: ' .2;
+ echo 'Ngày Hết Hạn: ' .3;
+ echo 'Số Thẻ: ' .4;
+ echo 'id pubg: ' .5;
+</body>
+</html>
+ 
